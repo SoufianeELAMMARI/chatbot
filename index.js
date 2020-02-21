d3c780596151f93ee943d712fb3dd3c3672fad63
Soufiane ELAMMARI <elammarisoufiane@gmail.com>
2020-02-22 00:16:21 +0100

    com

--- index.js
+++ index.js
@@ -13,3 +13,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => console.log('Express server is listening on port ',PORT));
 app.get('/', verifyWebhook);
 
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+
+});
+
+//EAAMG5Fcw2fkBAMaVG5H1ZC4tUOfL8b9iYO9vFmD2wvb7IrczPTgS5LFuwCa3wInDyWKzkyZB1ZAdZAAZBB1X4nZCGhYKpmtesU6hb1Ft2PyFPd0qosFEYZBy0tOaH4j4KgtJFYDfYWE5uDc7OPZBzPWjDRYcTypp88P80hTZBujXZA4aNKYRe60k9F
